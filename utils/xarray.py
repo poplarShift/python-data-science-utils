2c433efbfbbd0bfc7aec5b15d54e3f5d3e816f6f
poplarShift <poplarShift@users.noreply.github.com>
2019-06-06 09:03:09 -0400

    init xarray, get_unique

---
+++ utils/xarray.py
@@ -0,0 +1,31 @@
+import numpy as np
+import xarray as xr
+import pandas as pd
+
+def get_unique(x, axis=0):
+    """
+    Return unique non-nan value along specified xarray axis.
+
+    Raises
+    ------
+    ValueError: if there are more than one non-nan values
+    """
+    is_dt = np.issubdtype(x.dtype, np.datetime64)
+    x_ = np.moveaxis(x, source=axis, destination=-1)
+    iteridx = x_.shape[:-1]
+    u = np.zeros(iteridx, dtype=x.dtype) if is_dt else np.nan*np.zeros(iteridx)
+
+    for i in np.ndindex(iteridx):
+        u_ = np.unique(x_[i])
+        nan_nat = pd.isnull(u_)
+        uu_ = u_[~nan_nat]
+        if len(uu_)==1:
+            u[i] = uu_[0]
+        elif pd.isnull(u_[0]):
+            if is_dt:
+                u[i] = np.datetime64('NaT')
+            else:
+                u[i] = np.nan
+        else:
+            raise ValueError('Non-unique slices encountered!')
+    return u

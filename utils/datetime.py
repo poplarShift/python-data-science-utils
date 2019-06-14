edb25e1ac31ba2855b01221e213d365d4d589f2d
poplarShift <poplarShift@users.noreply.github.com>
2019-06-14 11:51:38 -0400

    init datetime module

---
+++ utils/datetime.py
@@ -0,0 +1,7 @@
+import datetime as dt
+import numpy as np
+
+def matlab2datetime(matlab_datenum):
+    day = dt.datetime.fromordinal(int(matlab_datenum))
+    dayfrac = dt.timedelta(days=matlab_datenum%1) - dt.timedelta(days = 366)
+    return day + dayfrac

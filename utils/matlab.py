d044fe2c2ea1206b65193fd70bc9dfb0f238dbb7
poplarShift <poplarShift@users.noreply.github.com>
2019-10-03 12:35:51 -0400

    handle nans in datetime conversion from datenum format

--- utils/matlab.py
+++ utils/matlab.py
@@ -2,6 +2,9 @@ import datetime as dt
 import numpy as np
 
 def matlab2datetime(matlab_datenum):
-    day = dt.datetime.fromordinal(int(matlab_datenum))
-    dayfrac = dt.timedelta(days=matlab_datenum%1) - dt.timedelta(days = 366)
-    return day + dayfrac
+    if np.isnan(matlab_datenum):
+        return np.NaN
+    else:
+        day = dt.datetime.fromordinal(int(matlab_datenum))
+        dayfrac = dt.timedelta(days=matlab_datenum%1) - dt.timedelta(days = 366)
+        return day + dayfrac

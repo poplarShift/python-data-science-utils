3f8a0fdf358e5b959d006ba9d8bae43d12e73098
poplarShift <poplarShift@users.noreply.github.com>
2019-12-09 14:42:19 -0500

    new mpl functions

--- utils/mpl.py
+++ utils/mpl.py
@@ -2,6 +2,36 @@ import cartopy.crs as ccrs
 import matplotlib.ticker as mticker
 from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
 import numpy as np
+from matplotlib.transforms import Bbox
+from matplotlib.path import Path
+
+def cmap_to_list(cm, N=None, out='rgba'):
+    """
+    Turns a MPL colour map into a list of colors.
+
+    Parameters
+    ----------
+        N: number of colors to sample (equal steps across entire cmap)
+        out: 'rgba' or 'hex'
+
+    License
+    -------
+    GNU-GPLv3, (C) A. R.
+    (https://github.com/poplarShift/python-data-science-utils)
+    """
+    if N is None:
+        N = cm.N
+    if out == 'rgba':
+        cm2clr = lambda i: cm(i)
+    elif out == 'hex':
+        cm2clr = lambda i: mpl.colors.to_hex(cm(i))
+
+    return [cm2clr(i) for i in np.linspace(0, 1, N)]
+
+def align_xaxis_extents(anchor_ax, ax):
+    x0, w = anchor_ax.get_position().bounds[::2]
+    y0, h = ax.get_position().bounds[1::2]
+    ax.set_position(Bbox.from_bounds(x0, y0, w, h))
 
 def set_cartopy_grid(ax, lons, lats, label_opts=None, grid_opts=None, **kwargs):
     """
@@ -70,3 +100,40 @@ def set_cartopy_grid(ax, lons, lats, label_opts=None, grid_opts=None, **kwargs):
                 verticalalignment='top',
                 **label_opts
             )
+
+
+def circumpolar_axis(ax):
+    """
+    Draw a circumpolar grid of longitudes around a map at latitude 62 degrees N
+
+    License
+    -------
+    GNU-GPLv3, (C) A. R.
+    (https://github.com/poplarShift/python-data-science-utils)
+    """
+    circle = Path.circle(radius=3e6)
+    proj = ax.projection
+    ax.set_boundary(circle, transform=proj)
+    # collections are only clipped when added after this point, so re-add them
+    for c in ax.collections:
+        c.remove()
+        ax.add_collection(c)
+
+    lat = 62
+    def rotation(lon):
+        if abs(lon)<=90:
+            return lon
+        else:
+            return lon-180
+
+    for lon in np.arange(-180, 180, 60):
+        lon_text = '{:3d}$^\circ${}'.format(
+                        abs(lon),
+                        {True: 'E', False: 'W'}[lon>=0]
+                    )
+        textopts = dict(va='center', ha='center', rotation=rotation(lon))
+        ax.text(*proj.transform_point(lon, lat, ccrs.PlateCarree()), lon_text, **textopts)
+
+def squeeze_axis_upward(ax, newy=0.5):
+    x, y, w, h = ax.get_position().bounds
+    ax.set_position((x, newy, w, y+h-newy))

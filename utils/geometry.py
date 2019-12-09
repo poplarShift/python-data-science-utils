36d94bfd8665aadf59e4b2abdc932e389d533dcb
poplarShift <poplarShift@users.noreply.github.com>
2019-12-09 14:57:32 -0500

    smoothen shapely polygon boundaries

--- utils/geometry.py
+++ utils/geometry.py
@@ -1,6 +1,6 @@
 from scipy.spatial import cKDTree
 import numpy as np
-from shapely.geometry import Point
+from shapely.geometry import Point, Polygon
 from geopandas import GeoDataFrame
 
 # need to sort out geometry vs. attr vs dict entry/column,
@@ -95,3 +95,23 @@ def nearest_with_time_constraint(d1, d2, x, y, dist_tol=.1, t='date', t_tol=1):
                   for idx, dist, candidates
                   in zip(nearest, distances, within_t_tol)]
     return nearest, within_tol
+
+
+def smoothen(g):
+    """
+    Insert new points along Polygon boundary.
+
+    License
+    -------
+    GNU-GPLv3, (C) A. R.
+    (https://github.com/poplarShift/python-data-science-utils)
+    """
+    newcoords = []
+    for c in g.exterior.coords.xy:
+        newc = []
+        for c1, c2 in zip(c[:-1], c[1:]):
+            newc += list(np.linspace(c1, c2, 50)[:-1])
+        newcoords.append(newc)
+
+    newcoords = [(x,y) for x, y in zip(*newcoords)]
+    return Polygon(newcoords)

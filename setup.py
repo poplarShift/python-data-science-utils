758cdc29b879eb28d0d7d07dc57dddf0f151e259
poplarShift <poplarShift@users.noreply.github.com>
2020-08-24 10:16:42 -0400

    version from git describe

--- setup.py
+++ setup.py
@@ -1,7 +1,21 @@
 from setuptools import setup
 
+def get_git_version():
+    """
+    Use git describe to get version based on git tag.
+    Save output to file
+    """
+    from subprocess import check_output
+    output = check_output(['git', 'describe', '--dirty',]).decode()
+    version = output[:-1] # had trailing newline
+
+    with open('.version', 'w') as f:
+        f.write(version)
+
+    return version
+
 setup(name='utils',
-      version='0.1',
+      version=get_git_version(),
       description='Utils',
       author='Achim Randelhoff',
       packages=['utils',],

409754a88b3cf7b4880925e7419e899f21ed96a1
poplarShift <poplarShift@users.noreply.github.com>
2020-09-15 10:29:12 -0400

    do not write version file on install

--- setup.py
+++ setup.py
@@ -3,15 +3,10 @@ from setuptools import setup
 def get_git_version():
     """
     Use git describe to get version based on git tag.
-    Save output to file
     """
     from subprocess import check_output
     output = check_output(['git', 'describe', '--dirty',]).decode()
     version = output[:-1] # had trailing newline
-
-    with open('.version', 'w') as f:
-        f.write(version)
-
     return version
 
 setup(name='utils',
